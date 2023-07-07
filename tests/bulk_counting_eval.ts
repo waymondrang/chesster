bc893fb38f783133bccee2f070e3b4d6cc1b8e46
Raymond Wang <me@waymondrang.com>
2023-07-07 11:58:05 -0700

    update bulk counting eval test

--- tests/bulk_counting_eval.ts
+++ tests/bulk_counting_eval.ts
@@ -47,10 +47,12 @@ function countBulkPositions(depth: number): [number, number, number, number] {
       for (let j = 0; j < moves.length; j++) {
         game.move(moves[j]);
 
-        count += countBulkPositions(depth - 1)[0];
-        captures += countBulkPositions(depth - 1)[1];
-        checks += countBulkPositions(depth - 1)[2];
-        checkmates += countBulkPositions(depth - 1)[3];
+        let positions = countBulkPositions(depth - 1);
+
+        count += positions[0];
+        captures += positions[1];
+        checks += positions[2];
+        checkmates += positions[3];
 
         game.undo();
       }
@@ -60,26 +62,55 @@ function countBulkPositions(depth: number): [number, number, number, number] {
   return [count, captures, checks, checkmates];
 }
 
+function countBulkPositionsSimple(depth: number): number {
+  if (depth <= 0) return 1;
+
+  let count = 0;
+
+  for (let i = 0; i < boardSize; i++) {
+    if (game.board[i] !== 0 && (game.board[i] & 0b1) === game.turn) {
+      const moves = game.getAvailableMoves(i);
+      for (let j = 0; j < moves.length; j++) {
+        game.move(moves[j]);
+        count += countBulkPositionsSimple(depth - 1);
+        game.undo();
+      }
+    }
+  }
+
+  return count;
+}
+
 function measureCountBulkPositions(depth: number) {
   const startTime = performance.now();
 
-  const fen = "bqnb1rkr/pp3ppp/3ppn2/2p5/5P2/P2P4/NPP1P1PP/BQ1BNRKR w - - 2 9";
+  // const fen = "bqnb1rkr/pp3ppp/3ppn2/2p5/5P2/P2P4/NPP1P1PP/BQ1BNRKR w - - 2 9";
 
   // game.init(fenStringToGameState(fen));
 
-  const count = countBulkPositions(depth);
+  // const count = countBulkPositions(depth);
+  const count = countBulkPositionsSimple(depth);
 
+  // console.log(
+  //   "Depth: " +
+  //     depth +
+  //     "\tNumber of positions: " +
+  //     count[0] +
+  //     "\tNumber of captures: " +
+  //     count[1] +
+  //     "\tNumber of checks: " +
+  //     count[2] +
+  //     "\tNumber of checkmates: " +
+  //     count[3] +
+  //     "\tTime: " +
+  //     (performance.now() - startTime) +
+  //     "ms"
+  // );
   console.log(
     "Depth: " +
       depth +
       "\tNumber of positions: " +
-      count[0] +
-      "\tNumber of captures: " +
-      count[1] +
-      "\tNumber of checks: " +
-      count[2] +
-      "\tNumber of checkmates: " +
-      count[3] +
+      count +
       "\tTime: " +
       (performance.now() - startTime) +
       "ms"
